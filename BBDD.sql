d9867ffcca2ca280caa54f0eb875e5011d472a32
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-05-26 19:52:29 +0200

    Añadidos comentarios

--- BBDD.sql
+++ BBDD.sql
@@ -3,12 +3,15 @@ CREATE DATABASE DarkysGamesShop;
 USE DarkysGamesShop;
 */
 
+-- ELIMINACION DE TABLAS
 DROP TABLE IF EXISTS Videojuego_Categoria;
 DROP TABLE IF EXISTS Usuario_Videojuego;
 DROP TABLE IF EXISTS Categoria;
 DROP TABLE IF EXISTS Videojuego;
 DROP TABLE IF EXISTS Usuario;
+-- FIN ELIMINACION DE TABLAS
 
+-- CREACION DE TABLAS
 CREATE TABLE Categoria (
     id_categoria INT AUTO_INCREMENT PRIMARY KEY,
     nombre_categoria VARCHAR(50) UNIQUE
@@ -31,7 +34,7 @@ CREATE TABLE Videojuego_Categoria (
 CREATE TABLE Usuario (
     id_usuario INT AUTO_INCREMENT PRIMARY KEY,
     nombre_usuario VARCHAR(50),
-    contrasenya VARCHAR(50),
+    contrasena VARCHAR(50),
     email VARCHAR(50)
 );
 
@@ -42,7 +45,9 @@ CREATE TABLE Usuario_Videojuego (
     CONSTRAINT FK1_Usuario FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario),
     CONSTRAINT FK2_Videojuego FOREIGN KEY (id_videojuego) REFERENCES Videojuego(id_videojuego)
 );
+-- FIN CREACION DE TABLAS
 
+-- INSERTS DE PRUEBA
 INSERT INTO Videojuego (nombre_videojuego, precio) VALUES
 ("Portal 2", 9.75);
 
@@ -56,12 +61,13 @@ INSERT INTO Videojuego_Categoria VALUES
 (1, 1), -- Portal 2 | Plataformas
 (1, 2), -- Portal 2 | Puzles
 (1, 3), -- Portal 2 | Primera Persona
-(1, 4), -- Portal 2 | Divertidos
+(1, 4); -- Portal 2 | Divertidos
 
-INSERT INTO Usuario (nombre_usuario, contrasenya, email) VALUES
+INSERT INTO Usuario (nombre_usuario, contrasena, email) VALUES
 ("Darky485", "aF2840303", "jorgex1412@gmail.com");
 
 INSERT INTO Usuario_Videojuego VALUES (1, 1); -- Darky485 | Portal 2
+-- FIN DE INSERTS DE PRUEBA
 
 /*
 ALTER TABLE categoria AUTO_INCREMENT = 1;
