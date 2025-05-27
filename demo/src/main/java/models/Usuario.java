2a18747b87b6da01ee1d6eab73f671ccc4b39a86
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-05-27 09:34:31 +0200

    Nuevo constructor para usuario (temporal)

--- demo/src/main/java/models/Usuario.java
+++ demo/src/main/java/models/Usuario.java
@@ -9,6 +9,7 @@ public class Usuario extends UserSesion {
     private String email;
     private Videojuego[] videojuegos;
 
+    // CONSTRUCTOR CON TODOS LOS ATRIBUTOS
     public Usuario(int id_usuario, String nombre_usuario, String contrasena, String email, Videojuego[] videojuegos) {
         this.id_usuario = id_usuario;
         this.nombre_usuario = nombre_usuario;
@@ -17,6 +18,15 @@ public class Usuario extends UserSesion {
         this.videojuegos = videojuegos;
     }
 
+    // CONSTRUCTOR SIN LISTA DE VIDEOJUEGOS
+    public Usuario(int id_usuario, String nombre_usuario, String contrasena, String email) {
+        this.id_usuario = id_usuario;
+        this.nombre_usuario = nombre_usuario;
+        this.contrasena = contrasena;
+        this.email = email;
+    }
+
+    // CONSTRUCTOR SOLO CON EMAIL Y CONTRASEÑA
     public Usuario(String email, String contrasena) {
         this.email = email;
         this.contrasena = contrasena;
