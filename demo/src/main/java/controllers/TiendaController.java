0fe9b8fe7a27cd1b9371e340d5c89d7bace64a84
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-05-29 16:33:38 +0200

    Sistema para mostrar los botones dinamicos de los videojuegos

--- demo/src/main/java/controllers/TiendaController.java
+++ demo/src/main/java/controllers/TiendaController.java
@@ -1,18 +1,38 @@
 package controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import models.Videojuego;
 import org.example.demo.BBDD;
+import javafx.geometry.Insets;
 
 import java.util.ArrayList;
 
 public class TiendaController {
+    @FXML
+    private HBox videojuegosContainer;
 
     @FXML
     public void initialize() throws InterruptedException {
         ArrayList<Videojuego> listaVideojuegos = BBDD.getListaVideojuegos(0);
 
-        // IMPRIMIR LISTA DE VIDEOJUEGOS
+        for (Videojuego juego : listaVideojuegos) {
+            Button btn = new Button(juego.getNombre_videojuego());
+            btn.setPrefWidth(125);
+            btn.setPrefHeight(200);
+            btn.setStyle("-fx-background-color: #a100ff; -fx-text-fill: white;");
+
+            VBox.setMargin(btn, new Insets(10));
+
+            videojuegosContainer.getChildren().add(btn);
+        }
+    }
+}
+
+// IMPRIMIR LISTA DE VIDEOJUEGOS
+        /*
         for (int i = 0; i < listaVideojuegos.size(); i++) {
             System.out.println("ID     : " + listaVideojuegos.get(i).getId_videojuego());
             System.out.println("Nombre : " + listaVideojuegos.get(i).getNombre_videojuego());
@@ -25,5 +45,5 @@ public class TiendaController {
 
             System.out.println();
         }
-    }
-}
+
+         */
