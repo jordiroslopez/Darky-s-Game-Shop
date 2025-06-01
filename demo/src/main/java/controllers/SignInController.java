3da43d62bb0ed7b35639a1ecce6994f6e34f71ed
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-06-01 12:56:14 +0200

    cambio de texto LogOut

---
+++ demo/src/main/java/controllers/SignInController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class SignInController {
+
+    @FXML
+    public void signin(ActionEvent event) {
+
+    }
+}
