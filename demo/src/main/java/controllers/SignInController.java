6a6dd63d63ad59c3f5cf42a11601ad14a957024f
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-06-01 13:06:24 +0200

    Ventana de signin

--- demo/src/main/java/controllers/SignInController.java
+++ demo/src/main/java/controllers/SignInController.java
@@ -2,8 +2,21 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class SignInController {
+
+    @FXML
+    private TextField id_username;
+
+    @FXML
+    private TextField id_mail;
+
+    @FXML
+    private TextField id_pass;
+
+    @FXML
+    private TextField id_pass2;
 
     @FXML
     public void signin(ActionEvent event) {
