12020ffbbff827444ec6b6ab7dcab3a16434da95
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2025-06-02 09:48:59 +0200

    Sistema de verificacion de signin

--- demo/src/main/java/controllers/SignInController.java
+++ demo/src/main/java/controllers/SignInController.java
@@ -2,11 +2,14 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import org.example.demo.BBDD;
 import org.example.demo.SceneManager;
 
 public class SignInController {
+    @FXML
+    private Label lbl_error;
 
     @FXML
     private TextField id_username;
@@ -22,10 +25,26 @@ public class SignInController {
 
     @FXML
     public void signin(ActionEvent event) throws InterruptedException {
-        if (id_pass.getText().equals(id_pass2.getText())) {
-            if (BBDD.signin(id_username.getText(), id_pass.getText(), id_mail.getText())) { // VERIFICAR QUE EL USUARIO NO EXISTE Y CREAR CUENTA
-                SceneManager.canviarEscena("/org/example/demo/tienda-view.fxml", event);
+        if (id_pass.getText().equals(id_pass2.getText())) {                                                             // VERIFICAR QUE LAS CONTRASEÑAS COINCIDEN
+            if (!id_username.getText().equals("") && !id_pass.getText().equals("") && !id_mail.getText().equals("")) {  // VERIFICAR QUE LOS CAMPOS NO ESTEN VACIOS
+                if (BBDD.signin(id_username.getText(), id_pass.getText(), id_mail.getText())) {                         // VERIFICAR QUE EL EMAIL NO ESTE EN USO
+                    SceneManager.canviarEscena("/org/example/demo/tienda-view.fxml", event);
+                } else {
+                    lbl_error.setVisible(true);
+                    lbl_error.setText("Email en uso");
+                }
+            } else {
+                lbl_error.setVisible(true);
+                lbl_error.setText("Campos vacios");
             }
+        } else {
+            lbl_error.setVisible(true);
+            lbl_error.setText("Las contraseñas no coinciden");
         }
+    }
+
+    @FXML
+    public void login(ActionEvent event) {
+        SceneManager.canviarEscena("/org/example/demo/login-view.fxml", event);
     }
 }
